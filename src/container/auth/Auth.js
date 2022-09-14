a70e25e1c1345e941f06fd95a444cc8e34baea11
kishan7827 <kishanshingala17@gmail.com>
2022-09-14 11:20:35 +0530

    formik

--- src/container/auth/Auth.js
+++ src/container/auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import * as yup from 'yup';
+import { Form, Formik, useFormik } from 'formik';
 
 function Auth(props) {
     const [usertype, setuser] = useState('login');
@@ -26,26 +27,48 @@ function Auth(props) {
         nameref.current.style.border = '2px solid skyblue'
     }
 
-    let authschema;
+    let authschema={}, intval={};
 
     if (usertype === 'login' && reset === false) {
         authschema = {
             email: yup.string().required("please enter email.").email("please enter valid email."),
-            password: yup.string().required("please enter password.").min(8,'password must be 8 chracters long.'),
+            password: yup.string().required("please enter password.").min(8, 'password must be 8 chracters long.'),
         }
-    } else if (usertype === 'signup' && reset === false) {
+        intval = {
+            email: '',
+            password: ''
+        }
+    } else if (usertype === 'Signup' && reset === false) {
         authschema = {
             name: yup.string().required("please enter yor name."),
             email: yup.string().required("please enter email.").email("please enter valid email."),
-            password: yup.string().required("please enter password."),
+            password: yup.string().required("please enter password.").min(8, 'password must be 8 chracters long.'),
+        }
+        intval = {
+            name: '',
+            email: '',
+            password: '',
         }
     } else if (reset === true) {
         authschema = {
             email: yup.string().required("please enter email.").email("please enter valid email."),
         }
+        intval = {
+            email: ''
+        }
     }
 
     let schema = yup.object().shape(authschema);
+
+    const formikobj = useFormik({
+        initialValues: intval,
+        validationSchema: schema,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    const { errors, handleChange, handleSubmit } = formikobj;
 
     return (
         <div>
@@ -59,49 +82,53 @@ function Auth(props) {
                                 usertype === 'login' ? <h2>login</h2> : <h2>Signup</h2>
                         }
                     </div>
+                    <Formik values={formikobj} onSubmit={handleSubmit}>
+                        <Form className="php-email-form">
+                            {
+                                reset === true ? null :
+                                    usertype === 'login' ? null : <div className="row">
+                                        <div className="col-md-4 form-group">
+                                            <input onChange={handleChange} ref={nameref} type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                            <p>{errors.name}</p>
+                                            <div className="validate" />
+                                        </div>
+                                    </div>}
 
-                    <div action method="post" role="form" className="php-email-form">
-
-                        {
-                            reset === true ? null :
-                                usertype === 'login' ? null : <div className="row">
-                                    <div className="col-md-4 form-group">
-                                        <input ref={nameref} type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                        <div className="validate" />
-                                    </div>
-                                </div>}
-
-                        <div className="row">
-                            <div className="col-md-4 form-group mt-3 mt-md-0">
-                                <input ref={emailref} type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                                <div className="validate" />
-                            </div>
-                        </div>
-
-                        {
-                            reset === true ? null
-                                :
-                                <div className="row">
-                                    <div className="col-md-4 form-group mt-3 mt-md-0">
-                                        <input ref={passref} type="password" className="form-control" name="password" id="password" placeholder="Password" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                        <div className="validate" />
-                                    </div>
+                            <div className="row">
+                                <div className="col-md-4 form-group mt-3 mt-md-0">
+                                    <input onChange={handleChange} ref={emailref} type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                    <p>{errors.email}</p>
+                                    <div className="validate" />
                                 </div>
-                        }
+                            </div>
+
+                            {
+                                reset === true ? null
+                                    :
+                                    <div className="row">
+                                        <div className="col-md-4 form-group mt-3 mt-md-0">
+                                            <input onChange={handleChange} ref={passref} type="password" className="form-control" name="password" id="password" placeholder="Password" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                            <p>{errors.password}</p>
+                                            <div className="validate" />
+                                        </div>
+                                    </div>
+                            }
 
 
-                        {
-                            reset === true ?
-                                <div className="text-center"><button type="submit">Submit</button></div>
-                                :
-                                usertype === 'login' ? <div className="text-center"><button type="submit" onClick={() => { handlogin() }}>login</button></div> : <div className="text-center"><button type="submit" onClick={() => handsignup()}>Signup</button></div>
-                        }
+                            {
+                                reset === true ?
+                                    <div className="text-center"><button type="submit">Submit</button></div>
+                                    :
+                                    usertype === 'login' ? <div className="text-center"><button type="submit">login</button></div> : <div className="text-center"><button type="submit">Signup</button></div>
+                            }
 
-                        {usertype === 'login' ? <span>Creat a new account: <button className='btn' onClick={() => { setreset(false); setuser('Sigup') }}>Signup</button></span> : <span>Alrady have an account: <button className='btn' onClick={() => setuser('login')}>Login</button></span>}
-                        <br />
-                        <br />
-                        <span>Forgot password<button className='btn' onClick={() => setreset(true)}>Click Hear</button></span>
-                    </div>
+                        </Form>
+                    </Formik>
+                    {usertype === 'login' ? <span>Creat a new account: <button className='btn' onClick={() => { setreset(false); setuser('Signup') }}>Signup</button></span> : <span>Alrady have an account: <button className='btn' onClick={() => setuser('login')}>Login</button></span>}
+                    <br />
+                    <br />
+                    <span>Forgot password<button className='btn' onClick={() => setreset(true)}>Click Hear</button></span>
+
                 </div>
             </section>
         </div>
